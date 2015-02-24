f64c695b1aef18fa401c3bff5418acb425484dc4
abaade2 <ausbaade1@hotmail.com>
2015-02-23 19:09:39 -0600

    Update ParkingPass.java

--- src/unl/cse/project/ParkingPass.java
+++ src/unl/cse/project/ParkingPass.java
@@ -4,6 +4,8 @@ public class ParkingPass extends Product{
 	
 	private double hourlyFee;
 	private Venues venue;
+	private int hours;
+	private String date;
 	
 	
 	
@@ -12,7 +14,7 @@ public class ParkingPass extends Product{
 		public ParkingPass(String productCode, String productType, Venues venue, double hourlyFee){
 			super(productCode, productType);
 			this.hourlyFee = hourlyFee;
-			this.setVenue(venue);
+			this.venue = venue;
 		}
 		public double getHourlyFee() {
 			return hourlyFee;
@@ -21,13 +23,24 @@ public class ParkingPass extends Product{
 		public void setHourlyFee(double hourlyFee) {
 			this.hourlyFee = hourlyFee;
 		}
-		public Venues getVenue() {
-			return venue;
-		}
-		public void setVenue(Venues venue) {
-			this.venue = venue;
-		}
 
-
+		@Override
+		public void setHours(int hours){
+			this.hours = hours;
+		}
+		@Override
+		public void setDate(String date){
+			this.date = date;
+		}
+		public int getHours(){
+			return hours;
+		}
+		public String getDate(){
+			return date;
+		}
 		
+		public double getSubTotalPP(){
+			return this.hourlyFee * this.hours;
+		}
+			
 }
