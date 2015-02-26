6d04eef53d8f3d48395dcb9fb018945bba4245fb
abaade2 <ausbaade1@hotmail.com>
2015-02-25 22:34:04 -0600

    Update Customer.java

--- src/unl/cse/project/Customer.java
+++ src/unl/cse/project/Customer.java
@@ -18,13 +18,37 @@ public class Customer {
 		
 	}
 	
+	public double getDiscountPercentage() {
+		double discount = 0;
+	    if(this.type.equals("A")){
+	    	discount = 12;
+	        return discount;
+	    }else if(this.type.equals("M")){
+	    	discount = 7;
+	        return discount;
+	    }else{
+	    return discount;
+	    }
+	 }
+	
 	public double getFee(){
 		if(this.type == "M"){
 			return 120;
 		}else if(this.type == "A"){
 			return 150;
-		}else return 0;
-			
+		}else return 0;	
+	}
+
+	public void printCustomer(){
+		System.out.println(this.name + "(" + this.customerCode + ")");
+		System.out.println("[" + this.type +"]");
+		System.out.println(this.primaryContact.getName());
+		System.out.println(this.address);
+	}
+	
+	@Override
+	public String toString(){
+		return this.getName();
 	}
 	
 	public String getCustomerCode() {
