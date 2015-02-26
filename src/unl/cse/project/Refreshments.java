ee44a995916a73166b02728837654cb9dd754ba6
abaade2 <ausbaade1@hotmail.com>
2015-02-25 22:40:32 -0600

    Update Refreshments.java

--- src/unl/cse/project/Refreshments.java
+++ src/unl/cse/project/Refreshments.java
@@ -11,6 +11,14 @@ public class Refreshments extends Product{
 			this.name = name;
 			this.cost = cost;
 		}
+		@Override
+		public double getSubtotal(){
+			return this.cost * this.getItemQuantity();
+		}
+		@Override
+		public double getTax(){
+			return this.getSubtotal() * 0.04;
+		}
 		public double getCost() {
 			return cost;
 		}
@@ -22,6 +30,32 @@ public class Refreshments extends Product{
 		}
 		public void setName(String name) {
 			this.name = name;
+		}
+		@Override
+		public void printGameTicket() {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void printSeasonPass() {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void printParkingPass() {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void printPSL() {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void printRefreshments() {
+			System.out.printf("%s %10s (%d units @ $%.2f/unit) $%15.2f $%5.2f $%4.2f\n",
+					this.productCode, this.name, this.itemQuantity, this.cost, this.getSubtotal(), this.getTax(), this.getTotal());
+			
 		}
 		
 }
