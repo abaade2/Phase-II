4fe284c05cd4af3064ce38ca6613bc846695b00d
abaade2 <ausbaade1@hotmail.com>
2015-02-26 22:48:10 -0600

    Update PSL.java

--- src/unl/cse/project/PSL.java
+++ src/unl/cse/project/PSL.java
@@ -24,7 +24,8 @@ public class PSL extends Product{
 		public double getSubtotal(){
 			//item quantity of PSL * (price of 1 game ticket * .18) 
 			//plus cost of license(licensefee)
-			return (this.getItemQuantity() * (ticket.getPricePerUnit() * .18)) + this.licenseFee;
+			return (this.getItemQuantity() * (this.ticket.getPricePerUnit() * .18)) + this.licenseFee;
+		
 		}
 		
 		
@@ -63,22 +64,24 @@ public class PSL extends Product{
 			
 		}
 		
-		@Override
-		public String toString(){
-			String seatString = "";
-			String seat = "";
-			for(int i=0; i<seats.length; i++){
-				seat = seatString.concat(seats[i]);
+		public String seatToString(String[] seats){
+			StringBuffer result = new StringBuffer();
+			for (int i = 0; i < seats.length; i++) {
+				   result.append( seats[i] );
+				   if(i+1<seats.length){
+					   result.append(":"); 
+				   }
 			}
-			return seat;
+			String newstring = result.toString();
+			return newstring;			
 		}
 		
 		
 
 		@Override
 		public void printPSL() {
-			System.out.printf("%s %10s [%s](%d units @ 18% of %s with $%.2f fee) $%15.2f $%5.2f $%4.2f\n",
-					this.productCode, "PSL", this.itemQuantity, this.seats.toString() , this.ticket.getProductCode(), this.licenseFee, this.getSubtotal(), this.getTax(), this.getTotal());
+			System.out.printf("%s %10s [%s](%d units @ 18%% of %s with $%.2f fee) $%15.2f $%5.2f $%4.2f\n",
+					this.productCode, "PSL", seatToString(this.seats), this.itemQuantity , this.ticket.getProductCode(), this.licenseFee, this.getSubtotal(), this.getTax(), this.getTotal());
 		}
 
 		@Override
