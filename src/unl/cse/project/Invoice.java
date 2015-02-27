348ad54cf5f0a5157ab263479ec3adad2b91015d
abaade2 <ausbaade1@hotmail.com>
2015-02-26 22:44:58 -0600

    Update Invoice.java

--- src/unl/cse/project/Invoice.java
+++ src/unl/cse/project/Invoice.java
@@ -6,53 +6,73 @@ public class Invoice {
 private String invoiceCode;
 	private Customer customer;
 	private Persons salesPerson;
-	private String date;
+	private DateTime date;
 	private Product[] product;
 	
 	
 	public Invoice( String invoiceCode,
 	 Customer customer,
 	 Persons salesPerson,
-     String date,
+	 DateTime date,
 	Product[] product){
-		this.setInvoiceCode(invoiceCode);
-		this.setCustomer(customer);
-		this.setSalesPerson(salesPerson);
-		this.setDate(date);
-		this.setProduct(product);
+		this.invoiceCode = invoiceCode;
+		this.customer = customer;
+		this.salesPerson = salesPerson;
+		this.date = date;
+		this.product = product;
 	}
-
 	
+	@Override
 	public String toString(){
 		return this.invoiceCode;
 	}
 	public void printSummary(){
-		System.out.printf("%s %10s %10s %10.2f %10.2f %10.2f %10.2f\n", this.invoiceCode, this.customer.toString(), this.salesPerson.toString(), this.calculateSubtotal(), this.customer.getFee(), this.calculateTax(), this.calculateDiscount(), this.calculateFinal());
+		System.out.printf("%-10s %-20s %-20s $%10.2f $%6.2f $%10.2f $%10.2f $%10.2f\n", this.invoiceCode, this.customer.toString(), this.salesPerson.toString(), this.calculateSubtotal(), this.customer.getFee(), this.calculateTax(), this.calculateDiscount(), this.calculateFinal());
 	}
 
 	//calculates the total subtotal of all products on a specific invoice
 	public double calculateSubtotal(){
 		int i = 0;
 		double subtotal = 0;
-		
-		//for(i=0; i<product.length; i++){
-			//if the product is a refreshment 
-			/*if(product[i].getProductType().equals("SR")){		
-				for(Product pro: product){
-					//if there is a gameticket or seasonpass purchased in the invoice the refreshment has a 5% discount
-					//System.out.println(product[j].getProductCode());
-					if(pro.getProductType().equals("TS") || pro.getProductType().equals("TG")){
-						subtotal += (product[i].getSubtotal() * .05) - product[i].getSubtotal();
-					}*/
-							
-		//		}
-		//	}
-		//	else{
-				subtotal += product[i].getSubtotal();
-		//	}
-		//}
+		int check = 0;
+		for(Product prod: this.product){
+			//if the product is a refreshment
+			if(prod.getProductType().equals("SR")){
+				//check = 0;
+				//if the product is a refreshment, the item quantity is messed up when there are multiple invoices with the same refreshment at a different quantity
+				//while(check == 0){ //to end the for loop if the search finds a gameticket or seasopass(prevents readding the price of the refreshment)
+					for(Product pro: this.product){
+						//if there is a gameticket or seasonpass purchased in the invoice the refreshment has a 5% discount
+						if(pro.getProductType().equals("TS") || pro.getProductType().equals("TG")){
+							subtotal += (prod.getSubtotal() - (prod.getSubtotal() * 0.05)) * prod.getItemQuantity();
+							check++;
+						}			
+					}
+				//}
+				//if(check ==0){
+					//subtotal += prod.getSubtotal();
+				//}
+			}
+			if(prod.getProductType().equals("TS")){
+				double daysLeft = Days.daysBetween(this.date, prod.getEndDate()).getDays();
+				double costPerDay = prod.getSubtotal() / prod.calculateTotalDays();
+				//fixed rate prorated to the days left
+				double cost = costPerDay * daysLeft;
+				double unutilizedTime = prod.calculateTotalDays() - daysLeft;
+				//unutilized time * cost per day * 30%
+				double extraCost = unutilizedTime * costPerDay * .3;
+				//add extracost and cost
+				double sum = cost + extraCost;
+				//multiply by the item quantity
+				subtotal += sum * prod.getItemQuantity();
+			}
+			else{
+				subtotal += prod.getSubtotal();
+			}
+		}
 		return subtotal;
 	}
+	
 	
 	
 	
@@ -77,8 +97,8 @@ private String invoiceCode;
 	public double calculateTax(){
 		int i = 0;
 		double tax = 0;
-		for(i=0; i<product.length; i++){
-			tax += product[i].getTax();
+		for(i=0; i<this.product.length; i++){
+			tax += this.getProduct()[i].getTax();
 		}
 		return tax;
 	}
@@ -113,12 +133,12 @@ private String invoiceCode;
 	}
 
 
-	public String getDate() {
+	public DateTime getDate() {
 		return date;
 	}
 
 
-	public void setDate(String date) {
+	public void setDate(DateTime date) {
 		this.date = date;
 	}
 
@@ -151,5 +171,4 @@ private String invoiceCode;
 	public void setInvoiceCode(String invoiceCode) {
 		this.invoiceCode = invoiceCode;
 	}
-
 }
